1311fab0a9c80dba1488d26a26a461d03cdebd93
Andrea Baronchelli <baronchelli.andrea@gmail.com>
2021-04-19 13:56:10 +0200

    milestone 1

---
+++ js/script.js
@@ -0,0 +1,126 @@
+/****************************************************************
+ * DISPLAY ICONS
+****************************************************************/
+
+const icons = [
+    {
+        name: 'cat',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+    {
+        name: 'crow',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+    {
+        name: 'dog',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+    {
+        name: 'dove',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+    {
+        name: 'dragon',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+    {
+        name: 'horse',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+    {
+        name: 'hippo',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+    {
+        name: 'fish',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+    {
+        name: 'carrot',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas',
+    },
+    {
+        name: 'apple-alt',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas',
+    },
+    {
+        name: 'lemon',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas',
+    },
+    {
+        name: 'pepper-hot',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas',
+    },
+    {
+        name: 'user-astronaut',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas',
+    },
+    {
+        name: 'user-graduate',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas',
+    },
+    {
+        name: 'user-ninja',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas',
+    },
+    {
+        name: 'user-secret',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas',
+    },
+];
+
+const container = document.querySelector(".icons");
+printIcons(container, icons);
+
+/****************************************************************
+ * FUNCTIONS
+****************************************************************/
+
+/** 
+ * Stampare icone in html
+ */
+function printIcons (container, icons) {
+    let html = "";
+    icons.forEach((icon) => {
+        const {name, prefix, family} = icon;
+        html += 
+        `<div class="icon">
+            <i class="${family} ${prefix}${name}"></i>
+            <div class="name">${name}</div>
+        </div>`;
+        return html;
+    });
+    container.innerHTML = html;
+};
