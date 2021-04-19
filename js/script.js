93a16dab1ec95f66cf5e37a3b3e622051475f325
Andrea Baronchelli <baronchelli.andrea@gmail.com>
2021-04-19 14:20:33 +0200

    milestone 2

--- js/script.js
+++ js/script.js
@@ -100,9 +100,14 @@ const icons = [
         family: 'fas',
     },
 ];
-
+const colors = ["#f9bc60", "#abd1c6", "#e16162"];
 const container = document.querySelector(".icons");
-printIcons(container, icons);
+const types = getTypes(icons);
+const coloredIcons = colorIcons(icons, types, colors);
+
+// printIcons(container, icons);
+printIcons(container, coloredIcons);
+
 
 /****************************************************************
  * FUNCTIONS
@@ -114,13 +119,43 @@ printIcons(container, icons);
 function printIcons (container, icons) {
     let html = "";
     icons.forEach((icon) => {
-        const {name, prefix, family} = icon;
+        const {name, prefix, family, color} = icon;
         html += 
         `<div class="icon">
-            <i class="${family} ${prefix}${name}"></i>
+            <i class="${family} ${prefix}${name}" style="color: ${color}"></i>
             <div class="name">${name}</div>
         </div>`;
         return html;
     });
     container.innerHTML = html;
 };
+
+/** 
+ * Ricavare i type delle icons
+ */
+function getTypes(icons) {
+    const types = [];
+    icons.forEach((icon) => {
+        if(! types.includes(icon.type) ) {
+            types.push(icon.type);
+        }
+    });
+    return types
+};
+
+/** 
+ * Colorare le icone
+*/
+function colorIcons(icons, types, colors) {
+    const coloredIcons = icons.map((icon) => {
+        const index = types.indexOf(icon.type);
+        return {
+            ...icon,
+            color: colors[index],
+        }
+    });
+    return coloredIcons;
+};
+
+
+
